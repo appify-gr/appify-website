4ac91b2c3c320c93cc907db0bc0bfd0026b11044
Vaggelis Magonezos <nospipian@gmail.com>
2025-07-23 11:21:25 +0300

    add favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,6 +23,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <link rel="icon" href="/favicon.ico" type="image/x-icon" />
       <body className={roboto.className}>
         {/* Navigation */}
         <nav className="sticky top-0 z-50 bg-gray-800/80 backdrop-blur-md border-b border-gray-700/50">
